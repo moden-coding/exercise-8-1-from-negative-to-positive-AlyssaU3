dea8a191b11eda4f195135f368f975c4563e17c4
AlyssaU3 <alys4321c@gmail.com>
2025-09-02 15:55:35 -0400

    exersise 8.1 done

--- src/negative_to_positive.py
+++ src/negative_to_positive.py
@@ -1,1 +1,11 @@
 # Write your solution here
+N= int(input("Please type in a positive integer: "))
+S =range(-N,0)
+for n in S:
+    print(n)
+L= range(1,N+1)
+for i in L:
+    print (i)
+
+#print(list(S), list(L))
+
